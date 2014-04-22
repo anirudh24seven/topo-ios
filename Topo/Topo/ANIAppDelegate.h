ba48df0262076f389a181aef17480ba8574a8170
Anirudh S <anirudh24seven@gmail.com>
2014-04-22 20:09:54 +0530

    Initial app commit

---
+++ Topo/Topo/ANIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANIAppDelegate.h
+//  Topo
+//
+//  Created by anirudh-MacBookAir on 22/04/14.
+//  Copyright (c) 2014 2Robots. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Topo/Topo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Topo
+//
+//  Created by anirudh-MacBookAir on 22/04/14.
+//  Copyright (c) 2014 2Robots. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ANIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ANIAppDelegate class]));
+    }
+}
